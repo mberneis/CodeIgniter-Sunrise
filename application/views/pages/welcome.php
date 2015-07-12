e9046bd768256e192d987a48bd5e856e30f0b062
mberneis <mberneis@gmail.com>
2015-07-12 12:30:45 -0400

    first try

--- application/views/pages/welcome.php
+++ application/views/pages/welcome.php
@@ -1,6 +1,10 @@
 <div class="container">
 	<div class="hero-unit">
 		<h1>Welcome to CodeIgniter Sunrise</h1>
-		<p><?php echo date("H:i:s"); ?></p>
+		<p><?php 
+			echo date("H:i:s") . "<pre>";
+			print_r ($_SERVER);
+		 	?>
+		 </p>
 	</div>
 </div>
