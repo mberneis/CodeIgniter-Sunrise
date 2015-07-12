c9830b8dea9b8fd2b23adb6e4c0e108e70299208
mberneis <mberneis@gmail.com>
2015-07-12 12:14:18 -0400

    can deploy

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -3,6 +3,7 @@
 class Welcome extends CI_Controller {
 
   function index() {
+  	error_log("Logging an error at ".date("m/d/Y H:i:s"));
     $this->load->view('header');
     $this->load->view('pages/welcome');
     $this->load->view('footer');
--- application/views/pages/welcome.php
+++ application/views/pages/welcome.php
@@ -1,5 +1,6 @@
 <div class="container">
 	<div class="hero-unit">
 		<h1>Welcome to CodeIgniter Sunrise</h1>
+		<p><?php echo date("H:i:s"); ?></p>
 	</div>
 </div>
